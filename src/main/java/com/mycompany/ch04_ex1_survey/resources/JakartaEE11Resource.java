6f09713b787d8b9e1f283a702cdd567ff79a80d0
Quoc Huy <23110103@student.hcmute.edu.vn>
2025-11-11 15:21:55 +0700

    first commit

---
+++ src/main/java/com/mycompany/ch04_ex1_survey/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package com.mycompany.ch04_ex1_survey.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
